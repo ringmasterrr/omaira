64c792a45f61298c31a3a174f56b3770e69d91b3
theringmasterrr <thevagish@gmail.com>
2024-05-01 22:32:35 +0530

    updates

--- components/ui/customDropdown.tsx
+++ components/ui/customDropdown.tsx
@@ -18,7 +18,7 @@ export default function CustomDropdown() {
 
 
       </DropdownTrigger>
-      <DropdownMenu className="translate-x-16 bg-white outline-none shadow-2xl" aria-label="Link Actions" >
+      <DropdownMenu className="translate-x-14 bg-white outline-none shadow-2xl rounded" aria-label="Link Actions" >
         <DropdownItem className="py-1 px-2 cursor-pointer hover:bg-slate-100 hover:border-slate-200 border-b text-base rounded-sm "> <Link href="/Tokenization-of-Real-Estate">Tokenization of Real Estate</Link> </DropdownItem>
         <DropdownItem className="py-1 px-2cursor-pointer hover:bg-slate-100 hover:border-slate-200 border-b text-base rounded-sm "> <Link href="/Tokenization-of-Mines">Tokenization of Mines</Link> </DropdownItem>
         <DropdownItem className="py-1 px-2 cursor-pointer hover:bg-slate-100 hover:border-slate-200 border-b text-base rounded-sm "> <Link href="/Tokenization-of-Agro-Farms">Tokenization of Agro Farms</Link> </DropdownItem>
